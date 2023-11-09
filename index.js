48698c5afbab379e51eabfaf7cbcff7beb6fb072
mhinton123 <matthewhinton@hotmail.co.uk>
2023-11-09 12:20:56 +0000

    HTML CSS implemented

---
+++ index.js
@@ -0,0 +1,11 @@
+const apiURL = `https://www.thecolorapi.com/scheme?hex=0047AB`
+
+// fetch(apiURL)
+//     .then(res => res.json())
+//     .then(data => {
+//         data.colors.forEach(color => {
+//             console.log(color.hex.value)
+//         });
+//     })
+
+   
